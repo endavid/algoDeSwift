43cb072a4c9052aa0fbdb2b9956c0997dd8cadbe
David Gavilan <david@endavid.com>
2024-02-16 15:01:00 +0000

    Example macOS App from Xcode template

---
+++ Algo de Example App/Algo de Example App/Algo_de_Example_App.swift
@@ -0,0 +1,17 @@
+//
+//  Algo_de_Example_App.swift
+//  Algo de Example App
+//
+//  Created by David Gavilan Ruiz on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Algo_de_Example_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Algo de Example App/Algo de Example App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Algo de Example App
+//
+//  Created by David Gavilan Ruiz on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
